8da37ad29601e74f694fcda891b262181badd9a3
soyacen <soyacen@outlook.com>
2021-06-11 14:03:31 +0800

    add NotContainsString

--- sliceutils/contains.go
+++ sliceutils/contains.go
@@ -4,3 +4,8 @@ package sliceutils
 func ContainsString(slice []string, value string) bool {
 	return IndexOfString(slice, value, 0) != IndexNotFound
 }
+
+// NotContainsString Checks if the value is not in the given slice.
+func NotContainsString(slice []string, value string) bool {
+	return IndexOfString(slice, value, 0) != IndexNotFound
+}

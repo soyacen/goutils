e18c75bb2c2e5b02518db0bd18c0a7282dce7c00
soyacen <soyacen@outlook.com>
2021-07-22 20:45:25 +0800

    fix bug

--- errorutils/recover.go
+++ errorutils/recover.go
@@ -1,12 +1,12 @@
 package errorutils
 
 // PanicHandler is called for recovering from panics spawned internally to the library (and thus
-// not recoverable by the caller's goroutine). Defaults to nil, which means panics are not recovered.
-var PanicHandler func(interface{})
+// not recoverable by the caller's goroutine).
+type PanicHandler func(interface{})
 
-func WithRecover(fn func()) {
+func WithRecover(handler PanicHandler, fn func()) {
 	defer func() {
-		handler := PanicHandler
+		// if handler to nil, which means panics are not recovered.
 		if handler != nil {
 			if err := recover(); err != nil {
 				handler(err)

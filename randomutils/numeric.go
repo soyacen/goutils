04183eb276d400f125bdce5b4108544e00789148
soyacen <soyacen@outlook.com>
2021-08-21 16:19:26 +0800

    add PickInt32

--- randomutils/numeric.go
+++ randomutils/numeric.go
@@ -12,11 +12,19 @@ func init() {
 	r = rand.New(rand.NewSource(time.Now().UnixNano()))
 }
 
+func Intn(n int) int {
+	return r.Intn(n)
+}
+
 // NumericPermString Generate a random number sequence of a given length
 func NumericPermString(length int) string {
 	var bs []byte
 	for i := 0; i < length; i++ {
-		bs = strconv.AppendInt(bs, int64(rand.Intn(10)), 10)
+		bs = strconv.AppendInt(bs, int64(Intn(10)), 10)
 	}
 	return string(bs)
+}
+
+func PickInt32(n ...int32) int32 {
+	return n[Intn(len(n))]
 }

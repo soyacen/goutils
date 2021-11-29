f7a99c636f0a655825b1e04a30668da5cc023dfb
soyacen <soyacen@outlook.com>
2021-11-29 18:30:24 +0800

    fix Signal waiter

--- signalutils/waiter.go
+++ signalutils/waiter.go
@@ -12,52 +12,64 @@ import (
 type SignalHook = map[os.Signal]func()
 
 type SignalWaiter struct {
-	signalC          chan os.Signal
 	signals          []os.Signal
+	signalC          chan os.Signal
+	ctx              context.Context
+	cancelFunc       context.CancelFunc
 	incomingSignal   os.Signal
 	hooks            []func(os.Signal)
-	stopC            chan interface{}
 	waitUntilTimeout time.Duration
+	stopC            chan interface{}
 	sync.RWMutex
 }
 
 type Option func(*SignalWaiter)
 
 func Signals(signals ...os.Signal) Option {
-	return func(helper *SignalWaiter) {
-		helper.signals = make([]os.Signal, 0)
+	return func(waiter *SignalWaiter) {
+		waiter.signals = make([]os.Signal, 0)
 		for _, signal := range signals {
-			helper.signals = append(helper.signals, signal)
+			waiter.signals = append(waiter.signals, signal)
 		}
 	}
 }
 
+func Context(parent context.Context) Option {
+	return func(waiter *SignalWaiter) {
+		waiter.ctx, waiter.cancelFunc = context.WithCancel(parent)
+	}
+}
+
 func Hooks(hooks ...func(os.Signal)) Option {
-	return func(helper *SignalWaiter) {
+	return func(waiter *SignalWaiter) {
 		for _, hook := range hooks {
-			helper.hooks = append(helper.hooks, hook)
+			waiter.hooks = append(waiter.hooks, hook)
 		}
 	}
 }
 
 func WaitUntilTimeout(d time.Duration) Option {
-	return func(helper *SignalWaiter) {
-		helper.waitUntilTimeout = d
+	return func(waiter *SignalWaiter) {
+		waiter.waitUntilTimeout = d
 	}
 }
 
 func NewSignalWaiter(opts ...Option) *SignalWaiter {
+	ctx, cancelFunc := context.WithCancel(context.Background())
 	w := &SignalWaiter{
-		signalC:          make(chan os.Signal),
 		signals:          ShutdownSignals(),
+		signalC:          make(chan os.Signal),
+		ctx:              ctx,
+		cancelFunc:       cancelFunc,
+		incomingSignal:   nil,
 		hooks:            make([]func(os.Signal), 0),
-		stopC:            make(chan interface{}, 1),
 		waitUntilTimeout: 30 * time.Second,
+		stopC:            make(chan interface{}, 1),
+		RWMutex:          sync.RWMutex{},
 	}
 	for _, opt := range opts {
 		opt(w)
 	}
-	w.signals = append(w.signals, syscall.SIGUSR1)
 	signal.Notify(w.signalC, w.signals...)
 	return w
 }
@@ -68,71 +80,41 @@ func (w *SignalWaiter) AddHook(f func(os.Signal)) {
 	w.hooks = append(w.hooks, f)
 }
 
-func (w *SignalWaiter) Leave() {
-	// leave
-	syscall.Kill(syscall.Getpid(), syscall.SIGUSR1)
+func (w *SignalWaiter) Kill(signum syscall.Signal) error {
+	return syscall.Kill(syscall.Getpid(), signum)
 }
 
 func (w *SignalWaiter) WaitSignals() *SignalWaiter {
-	// wait signals
 	w.incomingSignal = <-w.signalC
+	w.cancelFunc()
 	return w
 }
 
 func (w *SignalWaiter) AsyncInvokeHooks() *SignalWaiter {
-	// async invoke hooks
 	go func(sig os.Signal) {
 		w.RLock()
 		defer w.RUnlock()
 		defer close(w.stopC)
-		for _, f := range w.hooks {
-			defer f(sig)
+		for i := range w.hooks {
+			f := w.hooks[len(w.hooks)-1-i]
+			f(sig)
 		}
 	}(w.incomingSignal)
 	return w
 }
 
 func (w *SignalWaiter) WaitUntilTimeout() {
-	// wait until timeout
 	select {
 	case <-w.stopC:
 		return
 	case w.incomingSignal = <-w.signalC:
 		return
 	case <-time.After(w.waitUntilTimeout):
+		return
 	}
 }
 
-// SignalContext creates a new context  that cancels on the waiter's signals.
-func (w *SignalWaiter) SignalContext() (context.Context, context.CancelFunc) {
-	return SignalContext(w.signals...)
-}
-
-func ShutdownSignals() []os.Signal {
-	return []os.Signal{
-		syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGKILL, syscall.SIGUSR1,
-	}
-}
-
-// SignalContext creates a new context that cancels on the given signals.
-func SignalContext(signals ...os.Signal) (context.Context, context.CancelFunc) {
-	return ContextWithSignal(context.Background(), signals...)
-}
-
-// ContextWithSignal creates a new context that cancels on the given signals.
-func ContextWithSignal(ctx context.Context, signals ...os.Signal) (context.Context, context.CancelFunc) {
-	ctx, closer := context.WithCancel(ctx)
-
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, signals...)
-
-	go func() {
-		select {
-		case <-c:
-			closer()
-		case <-ctx.Done():
-		}
-	}()
-
-	return ctx, closer
+// Context return context that cancels on the waiter's signals.
+func (w *SignalWaiter) Context() context.Context {
+	return w.ctx
 }

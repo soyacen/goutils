45b0beac4b74892caaec025d3753d57989b3277a
soyacen <soyacen@outlook.com>
2021-12-03 16:58:17 +0800

    add runner option

--- runutils/run.go
+++ runutils/run.go
@@ -8,15 +8,45 @@ import (
 )
 
 type Runner struct {
-	start       func(ctx context.Context) error
-	startCtx    context.Context
-	stop        func(ctx context.Context) error
-	stopCtx     context.Context
-	waitTime    time.Duration
-	recoverFunc func(v interface{}, stack []byte)
+	start    func(ctx context.Context) error
+	startCtx context.Context
+	stop     func(ctx context.Context) error
+	stopCtx  context.Context
+	waitTime time.Duration
+	recover  func(v interface{})
 }
 
 type Option func(r *Runner)
+
+func StartCtx(ctx context.Context) Option {
+	return func(r *Runner) {
+		r.startCtx = ctx
+	}
+}
+
+func Stop(method func(ctx context.Context) error) Option {
+	return func(r *Runner) {
+		r.stop = method
+	}
+}
+
+func StopCtx(ctx context.Context) Option {
+	return func(r *Runner) {
+		r.stopCtx = ctx
+	}
+}
+
+func WaitTime(t time.Duration) Option {
+	return func(r *Runner) {
+		r.waitTime = t
+	}
+}
+
+func Recover(method func(v interface{})) Option {
+	return func(r *Runner) {
+		r.recover = method
+	}
+}
 
 func NewRunner(start func(ctx context.Context) error, opts ...Option) *Runner {
 	r := &Runner{
@@ -27,9 +57,9 @@ func NewRunner(start func(ctx context.Context) error, opts ...Option) *Runner {
 		},
 		stopCtx:  context.Background(),
 		waitTime: 0,
-		recoverFunc: func(v interface{}, stack []byte) {
+		recover: func(v interface{}) {
 			log.Printf("panic: %s\n", v)
-			log.Println(string(stack))
+			debug.PrintStack()
 		},
 	}
 	for _, option := range opts {
@@ -42,9 +72,11 @@ func (r *Runner) Run() error {
 	errC := make(chan error, 1)
 
 	go func() {
-		if v := recover(); v != nil {
-			r.recoverFunc(v, debug.Stack())
-		}
+		defer func() {
+			if v := recover(); v != nil {
+				r.recover(v)
+			}
+		}()
 		if err := r.start(r.startCtx); err != nil {
 			errC <- err
 		}
